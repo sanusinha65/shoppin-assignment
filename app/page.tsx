3e9cb1a0c5ea9d43ac905c1cc3fa52c4e1fefe30
Aditya Dhanraj <sanusinha65@gmail.com>
2024-12-29 23:59:05 +0530

    Production Issues Fixed

--- app/page.tsx
+++ app/page.tsx
@@ -225,7 +225,7 @@ export default function Home() {
                   {sizeOptions.map((sizeOption) => (
                     <div
                       key={sizeOption}
-                      className={`w-full max-w-xs md:max-w-md h-80 md:h-[448px] relative ${selectedSize === sizeOption ? 'border-blue-400 border-2' : ''
+                      className={`w-full max-w-xs md:max-w-md h-52 md:h-[448px] relative ${selectedSize === sizeOption ? 'border-blue-400 border-2' : ''
                         }`}
                       ref={(el) => {
                         if (selectedSize === sizeOption && el) {
@@ -284,7 +284,7 @@ export default function Home() {
                 </div>
               ) : (
                 <div
-                  className={`w-full h-80 max-w-xs md:h-[448px] md:max-w-md ${animateImage ? 'animate-up' : ''
+                  className={`w-full h-52 max-w-xs md:h-[448px] md:max-w-md ${animateImage ? 'animate-up' : ''
                     }`}
                 >
                   <div className="relative">
@@ -337,8 +337,8 @@ export default function Home() {
             </div>
 
             {/* Size selection buttons */}
-            <div className="w-full">
-              <div className="flex flex-row justify-center gap-4">
+            <div className="w-full max-w-screen-2xl">
+              <div className="flex flex-nowrap justify-center overflow-x-auto">
                 {isSizeButtonsSelected ? (
                   <div className="rounded-3xl text-[17px] flex bg-[#E8E8ED] px-5 py-2">
                     <button onClick={() => setIsSizeButtonsSelected(!isSizeButtonsSelected)}>
@@ -392,13 +392,14 @@ export default function Home() {
 
                 {isBandButtonsSelected ? (
                   <>
-                    <div className="rounded-3xl text-[17px] flex bg-[#E8E8ED] px-5 py-2">
+                    <div className="rounded-3xl flex flex-nowrap flex-row text-[17px] bg-[#E8E8ED] px-5 py-2">
                       <button onClick={() => setIsBandButtonsSelected(!isBandButtonsSelected)}>
                         <svg height="25" viewBox="0 0 10 25" width="10" xmlns="http://www.w3.org/2000/svg"><path d="m0 0h10v25h-10z" fill="none"></path><path d="m9.5 22.5a.5.5 0 0 1 -.5.5h-8a.5.5 0 1 1 0-1h.015a.485.485 0 0 0 .485-.485v-6.2216a4.5231 4.5231 0 0 0 1 .9448v5.2768a1.4779 1.4779 0 0 1 -.0813.485h5.1627a1.4758 1.4758 0 0 1 -.0814-.485v-5.2768a4.5209 4.5209 0 0 0 1-.9448v6.2216a.4851.4851 0 0 0 .4851.485h.0149a.5.5 0 0 1 .5.5zm-1.9194-19.5h-5.1621a1.4732 1.4732 0 0 1 .0815.485v9.015a2.5 2.5 0 0 0 5 0v-9.015a1.4873 1.4873 0 0 1 .0806-.485m1.4194-1a.5.5 0 0 1 .5.5.5.5 0 0 1 -.5.5h-.015a.485.485 0 0 0 -.485.485v9.015a3.5 3.5 0 0 1 -3.5 3.5 3.5 3.5 0 0 1 -3.5-3.5v-9.015a.485.485 0 0 0 -.485-.485h-.015a.5.5 0 0 1 0-1zm-3.2179 10.5a.75.75 0 1 0 -.75.75.75.75 0 0 0 .75-.75zm0-2.5a.75.75 0 1 0 -.75.75.75.75 0 0 0 .75-.75zm0-2.5a.75.75 0 1 0 -.75.75.75.75 0 0 0 .75-.75z" fill="#1d1d1f"></path></svg>
                       </button>
                       {watch.band.map((item, index) => (
                         <button
                           key={index}
+                          className="w-full whitespace-nowrap"
                           onClick={() => { setSelectedBand(item); setSelectedPrice(price[index]) }}>
                           <span className={`px-2 ${selectedBand === item ? 'font-bold' : ''}`}>{item}</span>
                         </button>
